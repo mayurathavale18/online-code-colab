8d7dfdfc3e2caadbc236b586cd57c14b7511079d
mayurathavale18 <mayurathavale.code100@protonmail.com>
2024-10-31 20:02:05 +0530

    added dynamic port

--- server/server.js
+++ server/server.js
@@ -132,6 +132,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-server.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000; // Use the PORT environment variable or default to 5000
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
