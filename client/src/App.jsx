37c0d3a6c232de11cd60e18e3748142262283130
mayurathavale18 <mayurathavale.code100@protonmail.com>
2024-10-31 20:08:32 +0530

    Configured Render Service URL in App.jsx socket URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://online-code-colab-final.onrender.com");
 
 const App = () => {
   const [roomId, setRoomId] = useState("");
